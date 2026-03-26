d553bd6665b67b3025ed3ff09af8d93859d95f3a
James-Dolinar <James-Dolinar@users.noreply.github.com>
2026-03-26 15:08:36 -0600

    Add featured courses carousel to courses page
    - Add a carousel of featured courses to the courses page
    - Use CardCarouselHydroshareFeatured to show featured courses

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,6 +100,7 @@ const config = {
     hs_logout_redirect: "https://portal.ciroh.org/contribute",
     hs_featured_apps_collection_id: process.env.HS_FEATURED_APPS_COLLECTION_ID || "dummy",
     hs_featured_datasets_collection_id: process.env.HS_FEATURED_DATASETS_COLLECTION_ID || "dummy",
+    hs_featured_courses_collection_id: process.env.HS_FEATURED_COURSES_COLLECTION_ID || "dummy",
 
     // URL for submitting a new product request (used in ProductCards component)
     productIssueUrl: "https://github.com/CIROH-UA/ciroh_hub/issues/new?template=product-request.md",
